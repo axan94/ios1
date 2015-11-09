e003c4d98ff3404baa07c01a03d236c425aa856a
Client1 <client1@mobileinno-client1s-mac-mini.local>
2015-11-09 11:07:14 +0100

    Initial Commit

---
+++ LastFM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LastFM
+//
+//  Created by Client1 on 09.11.15.
+//  Copyright © 2015 Client1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
